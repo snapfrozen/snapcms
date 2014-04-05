4bb09c9be0b20feca65798d95689d1d3e4fd876b
Francis Beresford <francis.beresford@gmail.com>
2014-04-05 17:13:28 +0800

    added db.php

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+	'connectionString' => 'mysql:host=localhost;dbname=galaxymap',
+	'emulatePrepare' => true,
+	'username' => 'root',
+	'password' => '',
+	'charset' => 'utf8',
+	'tablePrefix' => 'snap_',
+	//'enableProfiling' => true,
+	//'enableParamLogging' => true,
+);
