dca74211267ca50fb600f522ad8568395dbc86ee
Francis Beresford <francis@snapfrozen.com.au>
2015-01-05 10:22:49 +1100

    allow auto login by default.

--- backend/commands/views/frontend/config/backend.php
+++ backend/commands/views/frontend/config/backend.php
@@ -1,9 +1,13 @@
 <?php
+
 /**
  * Add or override backend configuration here
  */
 return array(
-	'components'=>array(
-		'db'=>require('../../db.php'),
-	)
+    'components' => array(
+        'db' => require('../../db.php'),
+        'user' => array(
+            'allowAutoLogin' => true
+        )
+    )
 );
