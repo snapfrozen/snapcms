bd577a6e1e7475dc8d00f48046aaef0000c649fd
Francis Beresford <francis@snapfrozen.com.au>
2015-03-07 21:58:42 +0700

    remove config from vendor.

--- components/Controller.php
+++ components/Controller.php
@@ -1,119 +1,121 @@
 <?php
+
 /**
  * Controller is the customized base controller class.
  * All controller classes for this application should extend from this base class.
  */
 class Controller extends CController
 {
-	/**
-	 * @var string the default layout for the controller view. Defaults to '//layouts/column1',
-	 * meaning using a single column layout. See 'protected/views/layouts/column1.php'.
-	 */
-	public $layout='//layouts/column1';
-	/**
-	 * @var array context menu items. This property will be assigned to {@link CMenu::items}.
-	 */
-	public $menu=array();
-	/**
-	 * @var array context menu items. This property will be assigned to {@link CMenu::items}.
-	 */
-	public $secondary_menu=array();
-	/**
-	 * @var string home label for the secondary nav
-	 */
-	public $nav_brand_label=false;
-	/**
-	 * @var mixed Yii url for secondary nav home label.
-	 */
-	public $nav_brand_url=false;
-	/**
-	 * @var array the breadcrumbs of the current page. The value of this property will
-	 * be assigned to {@link CBreadcrumbs::links}. Please refer to {@link CBreadcrumbs::links}
-	 * for more details on how to specify this property.
-	 */
-	public $breadcrumbs=array();
-	
-	public $page_heading;
-	public $page_heading_subtext;
-	
-	public $scriptLocations = array();
-        
-        public $meta_description = '';
-        public $meta_keywords = '';
-        public $meta_author = '';
-        
-	
-	public function init()
-	{
-		$baseUrl = Yii::app()->baseUrl;
-		$themeUrl = $baseUrl.'/'.Yii::app()->theme->baseUrl;
-		$this->scriptLocations[Yii::app()->theme->basePath] = $themeUrl;
-		
-		if(Yii::app()->request->pathInfo != 'content-type/status') 
-		{
-			$data = ContentType::findAll();
-			foreach($data as $ct) 
-			{ 
-				$ct->checkForErrors();
-				if($ct->hasSchemaErrors()) 
-				{
-					Yii::app()->user->setFlash('danger',
-						'<strong>Warning!</strong> Some content types database tables are not up to date. '.
-						CHtml::link('Please update them here.', array('contentType/status'))
-					);
-				}
-			}
-		}
-	}
+    /**
+     * @var string the default layout for the controller view. Defaults to '//layouts/column1',
+     * meaning using a single column layout. See 'protected/views/layouts/column1.php'.
+     */
+    public $layout = '//layouts/column1';
 
-	public function getModuleMenus($menuType)
-	{
-		$menu = array();
-		$App = Yii::app();
-		$Modules = $App->getModules();
-		foreach($Modules as $id=>$module)
-		{
-			$className = Yii::import($module['class']);
-			$Module = new $className($id,$App);
-			if(is_subclass_of($Module,'SnapCMSModule') && method_exists($Module,'getMenu'))
-				$menu = $Module->getMenu($menuType);
-			//$menu += array('label'=>$Module->name, 'url'=>array('/snapcms/'.$id), 'items'=>$Module->menu, 'visible'=>!Yii::app()->user->isGuest);
-		}
-		return $menu;
-	}
-	
-	public function createFrontendUrl($route, $params=array())
-	{
-		//@todo: must be a better way to do this..
-		$baseUrl = Yii::app()->baseUrl;
-		$backUrl = Yii::app()->urlManager->createUrl($route, $params, '&', true);
-		
-		//If the last character isn't admin.. we're in the frontend already
-		if(substr($baseUrl,-strlen('/admin')) !== '/admin')
-			return $backUrl;
-		
-		//@todo: must be a better way to do this..
-		$url = substr($baseUrl,0,-strlen('/admin')) . substr($backUrl,strlen($baseUrl)); //Removes admin/
-		return $url;
-	}
-	
-	public function createBackendUrl($route, $params=array())
-	{
-		//@todo: must be a better way to do this..
-		$baseUrl = Yii::app()->baseUrl;		
-		$frontUrl = Yii::app()->urlManager->createUrl($route, $params, '&', true);
-		
-		//If the last character are admin.. we're in the backend already
-		if(substr($baseUrl,-strlen('/admin')) == '/admin')
-			return $frontUrl;
-		
-		$frontUrl = substr($frontUrl,strlen($baseUrl));
-		$url = $baseUrl.'/admin'.$frontUrl; //Adds admin/
-		return $url;
-	}
-	
-	public function isEditable()
-	{
-		return Yii::app()->user->checkAccess('Update Content') ? true : false;
-	}
+    /**
+     * @var array context menu items. This property will be assigned to {@link CMenu::items}.
+     */
+    public $menu = array();
+
+    /**
+     * @var array context menu items. This property will be assigned to {@link CMenu::items}.
+     */
+    public $secondary_menu = array();
+
+    /**
+     * @var string home label for the secondary nav
+     */
+    public $nav_brand_label = false;
+
+    /**
+     * @var mixed Yii url for secondary nav home label.
+     */
+    public $nav_brand_url = false;
+
+    /**
+     * @var array the breadcrumbs of the current page. The value of this property will
+     * be assigned to {@link CBreadcrumbs::links}. Please refer to {@link CBreadcrumbs::links}
+     * for more details on how to specify this property.
+     */
+    public $breadcrumbs = array();
+    public $page_heading;
+    public $page_heading_subtext;
+    public $scriptLocations = array();
+    public $meta_description = '';
+    public $meta_keywords = '';
+    public $meta_author = '';
+
+    public function init()
+    {
+        $baseUrl = Yii::app()->baseUrl;
+        $themeUrl = $baseUrl . '/' . Yii::app()->theme->baseUrl;
+        $this->scriptLocations[Yii::app()->theme->basePath] = $themeUrl;
+
+        if (Yii::app()->request->pathInfo != 'content-type/status')
+        {
+            $data = ContentType::findAll();
+            foreach ($data as $ct)
+            {
+                $ct->checkForErrors();
+                if ($ct->hasSchemaErrors())
+                {
+                    Yii::app()->user->setFlash('danger', '<strong>Warning!</strong> Some content types database tables are not up to date. ' .
+                            CHtml::link('Please update them here.', array('contentType/status'))
+                    );
+                }
+            }
+        }
+    }
+
+    public function getModuleMenus($menuType)
+    {
+        $menu = array();
+        $App = Yii::app();
+        $Modules = $App->getModules();
+        foreach ($Modules as $id => $module)
+        {
+            $className = Yii::import($module['class']);
+            $Module = new $className($id, $App);
+            if (is_subclass_of($Module, 'SnapCMSModule') && method_exists($Module, 'getMenu'))
+                $menu = $Module->getMenu($menuType);
+            //$menu += array('label'=>$Module->name, 'url'=>array('/snapcms/'.$id), 'items'=>$Module->menu, 'visible'=>!Yii::app()->user->isGuest);
+        }
+        return $menu;
+    }
+
+    public function createFrontendUrl($route, $params = array())
+    {
+        //@todo: must be a better way to do this..
+        $baseUrl = Yii::app()->baseUrl;
+        $backUrl = Yii::app()->urlManager->createUrl($route, $params, '&', true);
+
+        //If the last character isn't admin.. we're in the frontend already
+        if (substr($baseUrl, -strlen('/admin')) !== '/admin')
+            return $backUrl;
+
+        //@todo: must be a better way to do this..
+        $url = substr($baseUrl, 0, -strlen('/admin')) . substr($backUrl, strlen($baseUrl)); //Removes admin/
+        return $url;
+    }
+
+    public function createBackendUrl($route, $params = array())
+    {
+        //@todo: must be a better way to do this..
+        $baseUrl = Yii::app()->baseUrl;
+        $frontUrl = Yii::app()->urlManager->createUrl($route, $params, '&', true);
+
+        //If the last character are admin.. we're in the backend already
+        if (substr($baseUrl, -strlen('/admin')) == '/admin')
+            return $frontUrl;
+
+        $frontUrl = substr($frontUrl, strlen($baseUrl));
+        $url = $baseUrl . '/admin' . $frontUrl; //Adds admin/
+        return $url;
+    }
+
+    public function isEditable()
+    {
+        return Yii::app()->user->checkAccess('Update Content') ? true : false;
+    }
+
 }
--- widgets/SnapGridView.php
+++ widgets/SnapGridView.php
@@ -1,157 +1,163 @@
 <?php
+
 Yii::import('bootstrap.widgets.BsGridView');
 Yii::import('backend.widgets.SnapDataColumn');
+
 class SnapGridView extends BsGridView
 {
-	public $selectableColumns = false;
-	public $selectedColumns = array();
-	public $defaultColumns = array();
-	protected $_selectableColumnsList = array();
-	
-	public $snapBaseScriptUrl = null;
-	
-	public function init()
-	{
-		$this->setFormatter(new SnapFormatter);
-		parent::init();
-		$this->snapBaseScriptUrl=Yii::app()->getAssetManager()->publish(Yii::getPathOfAlias('backend.widgets.assets')).'/snapgridview';
 
-		if($this->selectableColumns === true)
-		{
-			$cs=Yii::app()->getClientScript();
-			$cs->registerScriptFile($this->snapBaseScriptUrl.'/jquery.snapgridview.js',CClientScript::POS_END);
-			$cs->registerCssFile($this->snapBaseScriptUrl.'/styles.css');
-			$this->afterAjaxUpdate = "$.fn.snapGridView.init";
-			
-			$this->_setSelectedColumns();
-			
-			foreach($this->columns as $pos=>$column)
-			{
-				if(is_subclass_of($column, 'CDataColumn'))
-				{
-					//populate the _selectableColumnsList used for generating checkboxes
-					$this->_selectableColumnsList[$column->name] = 
-						$column->header ? 
-						$column->header : $this->dataProvider->model->getAttributeLabel($column->name);
-					
-					//remove any columns that arent' selected
-					if(!in_array($column->name, $this->selectedColumns)) {
-						unset($this->columns[$pos]);
-					}
-				}
-			}
-		}
-	}
-	
-	protected function _setSelectedColumns()
-	{
-		if(isset($_GET[$this->dataProvider->modelClass]['SelectedColumns'])) {
-			$this->selectedColumns = $_GET[$this->dataProvider->modelClass]['SelectedColumns'];
-			Yii::app()->session['SnapGridView_'.$this->dataProvider->modelClass.'_SelectedColumns'] = 
-				$this->selectedColumns;
-		} else if(isset(Yii::app()->session['SnapGridView_'.$this->dataProvider->modelClass.'_SelectedColumns'])) {
-			$this->selectedColumns = Yii::app()->session['SnapGridView_'.$this->dataProvider->modelClass.'_SelectedColumns'];
-		} else {
-			$this->selectedColumns = $this->defaultColumns;
-		}
-	}
-	
-	/**
-	 * Renders the data items for the grid view.
-	 */
-	public function renderItems()
-	{
-		if($this->dataProvider->getItemCount()>0 || $this->showTableOnEmpty)
-		{
-			if($this->selectableColumns) {
-				$this->renderColumnOptions();
-			}
-			echo "<table class=\"{$this->itemsCssClass}\">\n";
-			$this->renderTableHeader();
-			ob_start();
-			$this->renderTableBody();
-			$body=ob_get_clean();
-			$this->renderTableFooter();
-			echo $body; // TFOOT must appear before TBODY according to the standard.
-			echo "</table>";
-		}
-		else
-			$this->renderEmptyText();
-	}
-	
-	public function renderColumnOptions()
-	{
-		echo '<div class="filters snap-gv-column-options btn-group">';
-		echo CHtml::checkBoxList(
-			$this->dataProvider->modelClass.'[SelectedColumns]', 
-			$this->selectedColumns, 
-			$this->_selectableColumnsList,array(
-				'labelOptions'=>array(
-					'class'=>'btn btn-primary'
-				),
-				'template'=>'{label}{input}',
-				'separator'=>'',
-			));
-		echo '</div>';
-	}
-	
-	/**
-	 * Creates a {@link CDataColumn} based on a shortcut column specification string.
-	 * @param string $text the column specification string
-	 * @return CDataColumn the column instance
-	 */
-	protected function createDataColumn($text)
-	{
-		if(!preg_match('/^([\w\.]+)(:(\w*))?(:(.*))?$/',$text,$matches))
-			throw new CException(Yii::t('zii','The column must be specified in the format of "Name:Type:Label", where "Type" and "Label" are optional.'));
-		$column=new SnapDataColumn($this);
-		$column->name=$matches[1];
-		if(isset($matches[3]) && $matches[3]!=='')
-			$column->type=$matches[3];
-		if(isset($matches[5]))
-			$column->header=$matches[5];
-		return $column;
-	}
-	
-	/**
-	 * Creates column objects and initializes them.
-	 */
-	protected function initColumns()
-	{
-		if($this->columns===array())
-		{
-			if($this->dataProvider instanceof CActiveDataProvider)
-				$this->columns=$this->dataProvider->model->attributeNames();
-			elseif($this->dataProvider instanceof IDataProvider)
-			{
-				// use the keys of the first row of data as the default columns
-				$data=$this->dataProvider->getData();
-				if(isset($data[0]) && is_array($data[0]))
-					$this->columns=array_keys($data[0]);
-			}
-		}
-		$id=$this->getId();
-		foreach($this->columns as $i=>$column)
-		{
-			if(is_string($column))
-				$column=$this->createDataColumn($column);
-			else
-			{
-				if(!isset($column['class']))
-					$column['class']='SnapDataColumn';
-				$column=Yii::createComponent($column, $this);
-			}
-			if(!$column->visible)
-			{
-				unset($this->columns[$i]);
-				continue;
-			}
-			if($column->id===null)
-				$column->id=$id.'_c'.$i;
-			$this->columns[$i]=$column;
-		}
+    public $selectableColumns = false;
+    public $selectedColumns = array();
+    public $defaultColumns = array();
+    protected $_selectableColumnsList = array();
+    public $snapBaseScriptUrl = null;
 
-		foreach($this->columns as $column)
-			$column->init();
-	}
+    public function init()
+    {
+        $this->setFormatter(new SnapFormatter);
+        parent::init();
+        $this->snapBaseScriptUrl = Yii::app()->getAssetManager()->publish(Yii::getPathOfAlias('snapcms.widgets.assets')) . '/snapgridview';
+
+        if ($this->selectableColumns === true)
+        {
+            $cs = Yii::app()->getClientScript();
+            $cs->registerScriptFile($this->snapBaseScriptUrl . '/jquery.snapgridview.js', CClientScript::POS_END);
+            $cs->registerCssFile($this->snapBaseScriptUrl . '/styles.css');
+            $this->afterAjaxUpdate = "$.fn.snapGridView.init";
+
+            $this->_setSelectedColumns();
+
+            foreach ($this->columns as $pos => $column)
+            {
+                if (is_subclass_of($column, 'CDataColumn'))
+                {
+                    //populate the _selectableColumnsList used for generating checkboxes
+                    $this->_selectableColumnsList[$column->name] = $column->header ?
+                            $column->header : $this->dataProvider->model->getAttributeLabel($column->name);
+
+                    //remove any columns that arent' selected
+                    if (!in_array($column->name, $this->selectedColumns))
+                    {
+                        unset($this->columns[$pos]);
+                    }
+                }
+            }
+        }
+    }
+
+    protected function _setSelectedColumns()
+    {
+        if (isset($_GET[$this->dataProvider->modelClass]['SelectedColumns']))
+        {
+            $this->selectedColumns = $_GET[$this->dataProvider->modelClass]['SelectedColumns'];
+            Yii::app()->session['SnapGridView_' . $this->dataProvider->modelClass . '_SelectedColumns'] = $this->selectedColumns;
+        }
+        else if (isset(Yii::app()->session['SnapGridView_' . $this->dataProvider->modelClass . '_SelectedColumns']))
+        {
+            $this->selectedColumns = Yii::app()->session['SnapGridView_' . $this->dataProvider->modelClass . '_SelectedColumns'];
+        }
+        else
+        {
+            $this->selectedColumns = $this->defaultColumns;
+        }
+    }
+
+    /**
+     * Renders the data items for the grid view.
+     */
+    public function renderItems()
+    {
+        if ($this->dataProvider->getItemCount() > 0 || $this->showTableOnEmpty)
+        {
+            if ($this->selectableColumns)
+            {
+                $this->renderColumnOptions();
+            }
+            echo "<table class=\"{$this->itemsCssClass}\">\n";
+            $this->renderTableHeader();
+            ob_start();
+            $this->renderTableBody();
+            $body = ob_get_clean();
+            $this->renderTableFooter();
+            echo $body; // TFOOT must appear before TBODY according to the standard.
+            echo "</table>";
+        }
+        else
+            $this->renderEmptyText();
+    }
+
+    public function renderColumnOptions()
+    {
+        echo '<div class="filters snap-gv-column-options btn-group">';
+        echo CHtml::checkBoxList(
+                $this->dataProvider->modelClass . '[SelectedColumns]', $this->selectedColumns, $this->_selectableColumnsList, array(
+            'labelOptions' => array(
+                'class' => 'btn btn-primary'
+            ),
+            'template'     => '{label}{input}',
+            'separator'    => '',
+        ));
+        echo '</div>';
+    }
+
+    /**
+     * Creates a {@link CDataColumn} based on a shortcut column specification string.
+     * @param string $text the column specification string
+     * @return CDataColumn the column instance
+     */
+    protected function createDataColumn($text)
+    {
+        if (!preg_match('/^([\w\.]+)(:(\w*))?(:(.*))?$/', $text, $matches))
+            throw new CException(Yii::t('zii', 'The column must be specified in the format of "Name:Type:Label", where "Type" and "Label" are optional.'));
+        $column = new SnapDataColumn($this);
+        $column->name = $matches[1];
+        if (isset($matches[3]) && $matches[3] !== '')
+            $column->type = $matches[3];
+        if (isset($matches[5]))
+            $column->header = $matches[5];
+        return $column;
+    }
+
+    /**
+     * Creates column objects and initializes them.
+     */
+    protected function initColumns()
+    {
+        if ($this->columns === array())
+        {
+            if ($this->dataProvider instanceof CActiveDataProvider)
+                $this->columns = $this->dataProvider->model->attributeNames();
+            elseif ($this->dataProvider instanceof IDataProvider)
+            {
+                // use the keys of the first row of data as the default columns
+                $data = $this->dataProvider->getData();
+                if (isset($data[0]) && is_array($data[0]))
+                    $this->columns = array_keys($data[0]);
+            }
+        }
+        $id = $this->getId();
+        foreach ($this->columns as $i => $column)
+        {
+            if (is_string($column))
+                $column = $this->createDataColumn($column);
+            else
+            {
+                if (!isset($column['class']))
+                    $column['class'] = 'SnapDataColumn';
+                $column = Yii::createComponent($column, $this);
+            }
+            if (!$column->visible)
+            {
+                unset($this->columns[$i]);
+                continue;
+            }
+            if ($column->id === null)
+                $column->id = $id . '_c' . $i;
+            $this->columns[$i] = $column;
+        }
+
+        foreach ($this->columns as $column)
+            $column->init();
+    }
+
 }
