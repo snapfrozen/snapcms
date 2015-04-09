6281be4a6bdfc668f0de4e73032a2ec596402edd
Jackson Tong <jackson@snapfrozen.com.au>
2015-04-09 10:02:51 +0700

    Fixed lost file after update

--- controllers/ContentController.php
+++ controllers/ContentController.php
@@ -170,17 +170,24 @@ class ContentController extends Controller
 		{
 			$ContentType = $Content->ContentType;
 			$ContentType->loadData(); //Make sure data is loaded so that we dont' try create a new record.
-			$ContentType->attributes = $_POST['ContentType'];
-			
-			
+            $oldFile = array();
 
-			foreach($ContentType->fileFields as $field)
-			{
-				if(isset($_POST[$field.'_delete'])) {
-					$ContentType->$field = null;
-				}
-			}
-			
+            foreach($ContentType->fileFields as $field)
+            {
+                if(isset($_POST[$field.'_delete'])) {
+                    $ContentType->$field = null;
+                } else {
+                    $oldFile[$field] = $ContentType->$field;
+                }
+            }
+
+			$ContentType->attributes = $_POST['ContentType'];
+
+            foreach ($ContentType->fileFields as $field) {
+                if (empty($_POST[$field]) && !isset($_POST[$field.'_delete']))
+                    $ContentType->$field = $oldFile[$field];
+            }
+
 			$ContentType->save(); //Have to assume saved because function always returns 0;
 			
 			if(Yii::app()->request->isAjaxRequest)
