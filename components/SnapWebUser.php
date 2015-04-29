11d35cce935a366fe50ab58b07afca4da38c6b1d
Francis Beresford <francis@snapfrozen.com.au>
2015-04-29 12:29:44 +0700

    fix uploads path

--- components/SnapWebUser.php
+++ components/SnapWebUser.php
@@ -1,14 +1,14 @@
 <?php
 class SnapWebUser extends CWebUser
 {
-	protected function afterLogin($fromCookie)
-	{			
-		if($this->checkAccess('Update Content'))
-		{
-			$_SESSION['KCFINDER'] = array();
-			$_SESSION['KCFINDER']['disabled'] = false;
-			$_SESSION['KCFINDER']['uploadURL'] = "/uploads";
-			$_SESSION['KCFINDER']['uploadDir'] = "../../uploads/";
-		}
-	}
+        protected function afterLogin($fromCookie)
+        {
+                if($this->checkAccess('Update Content'))
+                {
+                        $_SESSION['KCFINDER'] = array();
+                        $_SESSION['KCFINDER']['disabled'] = false;
+                        $_SESSION['KCFINDER']['uploadURL'] = "/uploads";
+                        $_SESSION['KCFINDER']['uploadDir'] = Yii::getPathOfAlias('web').'/uploads';
+                }
+        }
 }
